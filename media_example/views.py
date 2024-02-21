2160f0457f0d168f5e7f13a334c73d7e16057b7a
jamesxpower <jamesxpower@gmail.com>
2024-02-21 17:29:54 +0800

    2024/02/21 study commit

--- media_example/views.py
+++ media_example/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.conf import settings
+from .forms import UploadForm
+from PIL import Image
 
-# Create your views here.
+def media_example(request):
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            save_path = settings.MEDIA_ROOT / form.cleaned_data["file_upload"] .name
+            image = Image.open(fp=form.cleaned_data["file_upload"])
+            image.thumbnail((50,50))
+            image.save(fp=save_path)
+
+            """
+            with open(save_path, 'wb+') as output_file:
+                for chunk in form.cleaned_data["file_upload"].chunks():
+                    output_file.write(chunk)
+            """
+    else:
+        form = UploadForm()
+
+    return render(request=request, template_name="media-example.html", context={"form": form})
+
